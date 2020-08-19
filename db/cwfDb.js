46a5dbb51e2c72db239a79a8079bec69d54e50d4
Mike Thurgood <mike.thurgood@wintechnologies.net>
2020-08-19 17:04:13 +0100

    db error fix

--- db/cwfDb.js
+++ db/cwfDb.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === 'development') {
     })
 }
 else  {
-    const sequelize = new Sequelize(process.env.DATABASE_URL)
+    sequelize = new Sequelize(process.env.DATABASE_URL)
 }
 
 
