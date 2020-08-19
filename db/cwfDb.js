e88ef467acc3ea5e7125c34dda40d9491c845459
Mike Thurgood <mike.thurgood@wintechnologies.net>
2020-08-19 16:56:03 +0100

    update db connection

--- db/cwfDb.js
+++ db/cwfDb.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize("CWF", "postgres", "Commenc4ly3t1!", {
-    dialect: "postgres", 
-    host: "localhost", 
-    port: 5432,
-    storage: "./session.sqlite"
-})
+// const sequelize = new Sequelize("CWF", "postgres", "Commenc4ly3t1!", {
+//     dialect: "postgres", 
+//     host: "localhost", 
+//     port: 5432,
+//     storage: "./session.sqlite"
+// })
+const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 module.exports = sequelize
