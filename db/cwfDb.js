6dca71da466d4201242399a72bc4fe6a1f9716b7
Mike Thurgood <mike.thurgood@wintechnologies.net>
2020-08-19 17:02:18 +0100

    added prod database

--- db/cwfDb.js
+++ db/cwfDb.js
@@ -1,10 +1,18 @@
 const Sequelize = require('sequelize')
-// const sequelize = new Sequelize("CWF", "postgres", "Commenc4ly3t1!", {
-//     dialect: "postgres", 
-//     host: "localhost", 
-//     port: 5432,
-//     storage: "./session.sqlite"
-// })
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+require('dotenv').config()
+
+let sequelize
+if (process.env.NODE_ENV === 'development') {
+    sequelize = new Sequelize(process.env.DBUSERNAME, "postgres", process.env.DBPASSWORD, {
+        dialect: "postgres", 
+        host: "localhost", 
+        port: 5432,
+        storage: "./session.sqlite"
+    })
+}
+else  {
+    const sequelize = new Sequelize(process.env.DATABASE_URL)
+}
+
 
 module.exports = sequelize
