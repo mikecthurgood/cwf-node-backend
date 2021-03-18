8dbfae7c12f5d1e4bf14548afd6abcbba8f6bcc0
mikecthurgood <mikecthurgood@gmail.com>
2021-03-18 12:00:11 +0000

    comment

--- util/file.js
+++ util/file.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+// leaving in for eventual adding and removing of profile images
 
 const clearImage = filePath => {
     filePath = path.join(__dirname, '..', filePath);
